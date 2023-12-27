b88804412f162c7c9a0dce39c1964c11c2bdb28c
Umarocks <umarocks@hotmail.com>
2023-12-27 01:10:32 -0600

    About section completed. still could make some minor changes if required

--- src/components/About/AboutCarts.jsx
+++ src/components/About/AboutCarts.jsx
@@ -1,29 +1,41 @@
 function AboutCarts() {
-  const cardList = {
-    first: {
+  const cardList = [
+    {
+      id: "first",
       img: "../../images/who-we-are/progresion.png",
       title: "Progression",
       description:
         "The point using fear lorem ipsum that aspimotive remal areat yearly oint rem ipsum ihaspe",
     },
-    second: {
+    {
+      id: "second",
       img: "../../images/who-we-are/workout.png",
       title: "Workout",
       description:
         "The point using fear lorem ipsum that aspimotive remal areat yearly oint rem ipsum ihaspe",
     },
-    third: {
+    {
+      id: "third",
       img: "../../images/who-we-are/nutritions.png",
       title: "Nutrition",
       description:
         "The point using fear lorem ipsum that aspimotive remal areat yearly oint rem ipsum ihaspe",
     },
-  };
+  ];
   return (
     <>
-      <div className="flex flex-col">
-        <img src={cardList.img} alt="box_img" />
-      </div>
+      {cardList.map((card, id) => (
+        <div
+          key={id}
+          className="flex flex-col bg-white justify-center p-6 text-center items-center mt-12 rounded-tl-[35px] rounded-br-[35px] shadow-xl min-h-[340px]"
+        >
+          <img src={card.img} alt="box_img" className=" w-[75px]" />
+          <h3 className=" text-[24px] font-bold uppercase">{card.title}</h3>
+          <p className="text-[16px] font-medium text-[#646464]">
+            {card.description}
+          </p>
+        </div>
+      ))}
     </>
   );
 }
